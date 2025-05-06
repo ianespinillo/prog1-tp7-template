2dcd06d84f937763638ce80cb31c12a0f12ef9f2
Ian Espinillo <ianespinillo@gmail.com>
2025-05-06 17:17:04 -0300

    TP-7

--- loops.py
+++ loops.py
@@ -1,18 +1,40 @@
 def index_of_by_index(word, list, index):
+    if index < 0 or index >= len(list):
+        return -1
+    for i in range(index, len(list)):
+        if list[i] == word:
+            return i
     return -1
+    
 
 
 def index_of_empty(list):
+    for i in range(len(list)):
+        if list[i] == "":
+            return i
     return -1
 
 
 def index_of(word, list):
-    return -1
+    if word in list:
+        return list.index(word)
+    else:
+        return -1
 
 
 def put(word, list):
-    return -1
+    if "" in list:
+        idx = list.index("")
+        list[idx] = word
+        return idx
+    else:
+        return -1
 
 
 def remove(word, list):
-    return -1
+    eliminated = 0
+    for i in range(len(list)):
+        if (list[i] == word):
+            list[i]=""
+            eliminated+=1
+    return eliminated
--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,10 @@
 def enumerate_list(list):
-    return list
-
-
+    new_list = [i for i in list if i != ""]
+    for i in range(len(new_list)):
+        new_list[i] = f"{i}. {new_list[i]}"
+    return new_list
 def enumerate_backwards(list):
-    return list
+    new_list = [i for i in list if i != ""]
+    for i in range(len(new_list)-1, -1, -1):
+        new_list[i] = f"{i}. {new_list[i][::-1]}"
+    return new_list
